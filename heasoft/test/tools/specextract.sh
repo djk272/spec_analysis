7dc18172d05d89b4559acaaf303112d0043c879d
Nikolai Shields <nikolai+github@nikolaishields.com>
2023-04-08 15:12:14 -0500

    New Workflow (#2)
    * cleanup repo
    
    * cleanup repo
    
    * Add makefile and remove for coming baseline commit
    
    * add make workflow

--- heasoft/test/tools/specextract.sh
+++
@@ -1,9 +0,0 @@
-specextract infile="evt2.fits[sky=region(ds9.reg)]" outroot=ds9 bkgfile="bkgrnd/merge_bg_RP.fits[sky=region(ds9.reg)]" grouptype=NUM_CTS binspec=20 bkgresp=no clobber=yes verbose=1 weight=no
-
-specextract infile="evt2.fits[sky=region(ds9.reg)]" outroot=ds9 bkgfile="bkgrnd/merge_bg_RP.fits[sky=region(ds9.reg)]" grouptype=NUM_CTS binspec=20 bkgresp=no clobber=yes verbose=1 weight=no
-
-specextract infile="evt2.fits[sky=region(ds9.reg)]" outroot=ds9 bkgfile="bkgrnd/merge_bg_RP.fits[sky=region(ds9.reg)]" grouptype=NUM_CTS binspec=20 bkgresp=no clobber=yes verbose=1 weight=no
-
-specextract infile="evt2.fits[sky=region(ds9.reg)]" outroot=ds9 bkgfile="bkgrnd/merge_bg_RP.fits[sky=region(ds9.reg)]" grouptype=NUM_CTS binspec=20 bkgresp=no clobber=yes verbose=1 weight=no
-
-specextract infile="evt2.fits[sky=region(ds9.reg)]" outroot=ds9 bkgfile="bkgrnd/merge_bg_RP.fits[sky=region(ds9.reg)]" grouptype=NUM_CTS binspec=20 bkgresp=no clobber=yes verbose=1 weight=no
